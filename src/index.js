921755e0eb4459326e991e77cbd40850b0b64aa5
AleksArden <i1723@i.ua>
2023-03-05 19:15:57 +0200

    add title and about project in the file readme

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="my-phonebook">
+        <BrowserRouter basename="phonebook">
           <ThemeProvider theme={theme}>
             <App />
           </ThemeProvider>

151d782ff296f4d74e8a3702e1d00d76e5a57d80
Oleksii Chubenko <i1723@i.ua>
2023-05-16 20:55:53 +0300

    fixed store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,28 +1,32 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
 import {
-    persistStore,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
-} from 'redux-persist'
-import { authReducer } from "./auth/auth.slice";
-import { contactsReducer } from "./contacts/contacts.slice";
-import { filterReducer } from "./filter/filter.slice";
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import { authReducer } from './auth/auth.slice';
+import { contactsReducer } from './contacts/contacts.slice';
+import { filterReducer } from './filter/filter.slice';
 
 export const store = configureStore({
-    reducer: {
-        contacts: contactsReducer,
-        auth: authReducer,
-        filter: filterReducer,
-    },
-    middleware: getDefaultMiddleware =>
-        getDefaultMiddleware({
-            serializableCheck: {
-                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-            },
-        }),
-})
+  reducer: {
+    contacts: contactsReducer,
+    auth: authReducer,
+    filter: filterReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;

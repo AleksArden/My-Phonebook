ad26f8f11f3368995791b9c536069bbb4dbb2ea7
Oleksii Chubenko <i1723@i.ua>
2023-05-17 19:42:44 +0300

    fixed ContactsPage

--- src/page/ContactsPage/ContactsPage.tsx
+++ src/page/ContactsPage/ContactsPage.tsx
@@ -2,8 +2,8 @@ import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import Filter from 'components/Filter/Filter';
 import ContactsList from 'components/ContactsList/ContactsList';
-import { getContactsThunk } from 'redux/contacts/contacts.thunk';
-import { useDispatch, useSelector } from 'react-redux';
+import { getContacts } from 'redux/contacts/contacts.thunk';
+import { useAppDispatch, useAppSelector } from 'redux/hooks/hooks';
 import { selectAuthToken } from 'redux/auth/auth.selector';
 import { selectAuthIsLoading } from 'redux/auth/auth.selector';
 import LinearColor from 'components/Skeleton/Skeleton';
@@ -14,14 +14,14 @@ import {
 import Error from 'components/Error/Error';
 
 const ContactsPage = () => {
-  const dispatch = useDispatch();
-  const isLoadingContact = useSelector(selectContactIsLoading);
-  const isLoadingAuth = useSelector(selectAuthIsLoading);
-  const error = useSelector(selectContactsError);
-  const token = useSelector(selectAuthToken);
+  const dispatch = useAppDispatch();
+  const isLoadingContact = useAppSelector(selectContactIsLoading);
+  const isLoadingAuth = useAppSelector(selectAuthIsLoading);
+  const error = useAppSelector(selectContactsError);
+  const token = useAppSelector(selectAuthToken);
 
   useEffect(() => {
-    dispatch(getContactsThunk());
+    dispatch(getContacts());
   }, [dispatch, token]);
   return (
     <>

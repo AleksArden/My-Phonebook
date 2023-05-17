823115778b69864192fbf605c4a4fff8b61d2e9f
Oleksii Chubenko <i1723@i.ua>
2023-05-17 23:15:05 +0300

    fixed ModalDelete

--- src/components/ModalDelete/ModalDelete.tsx
+++ src/components/ModalDelete/ModalDelete.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useAppDispatch, useAppSelector } from 'redux/hooks/hooks';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Button from 'components/Button/Button';
@@ -8,7 +8,7 @@ import {
   selectOpenModalDelete,
 } from 'redux/contacts/contacts.selector';
 import { closeModalDelete } from 'redux/contacts/contacts.slice';
-import { deleteContactThunk } from 'redux/contacts/contacts.thunk';
+import { deleteContact } from 'redux/contacts/contacts.thunk';
 
 import { Text, List } from './ModalDelete.styled';
 import { createPortal } from 'react-dom';
@@ -27,38 +27,40 @@ const style = {
 const modalRoot = document.querySelector('#modal-root');
 
 export default function ModalDelete() {
-  const contact = useSelector(selectCurrentContact);
+  const contact = useAppSelector(selectCurrentContact);
+  const dispatch = useAppDispatch();
 
-  const open = useSelector(selectOpenModalDelete);
+  const open = useAppSelector(selectOpenModalDelete);
   const handleDelete = () => {
-    dispatch(deleteContactThunk(contact.id));
+    dispatch(deleteContact(contact.id));
     dispatch(closeModalDelete());
   };
-  const dispatch = useDispatch();
   const handleClose = () => {
     dispatch(closeModalDelete());
   };
-  return createPortal(
-    <div>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style} style={{ backgroundColor: '#c0c0c0' }}>
-          <Text>Delete contact?</Text>
-          <List>
-            <li>
-              <Button name="Delete" color="error" onClick={handleDelete} />
-            </li>
-            <li>
-              <Button name="Cancel" color="primary" onClick={handleClose} />
-            </li>
-          </List>
-        </Box>
-      </Modal>
-    </div>,
-    modalRoot
-  );
+  return modalRoot
+    ? createPortal(
+        <div>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style} style={{ backgroundColor: '#c0c0c0' }}>
+              <Text>Delete contact?</Text>
+              <List>
+                <li>
+                  <Button name="Delete" color="error" onClick={handleDelete} />
+                </li>
+                <li>
+                  <Button name="Cancel" color="primary" onClick={handleClose} />
+                </li>
+              </List>
+            </Box>
+          </Modal>
+        </div>,
+        modalRoot
+      )
+    : null;
 }

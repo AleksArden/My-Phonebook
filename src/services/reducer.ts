ea7d3a756740e5aa682940866423a6b23109ccef
Oleksii Chubenko <i1723@i.ua>
2023-05-17 15:54:12 +0300

    fixed reducer.ts

--- src/services/reducer.ts
+++ src/services/reducer.ts
@@ -1,20 +1,56 @@
-export function formReducer(state, { type, payload }) {
-    return (state = { ...state, [type]: payload });
+interface StateAddContact {
+  name: string;
+  number: string;
 }
-
+type ActionAddContact = { type: 'name' | 'number'; payload: string };
 
 export const initStateAddContact = {
-    name: '',
-    number: '',
+  name: '',
+  number: '',
 };
+export function reducerAddContact(
+  state: StateAddContact,
+  { type, payload }: ActionAddContact
+) {
+  return (state = { ...state, [type]: payload });
+}
+
+interface StateLoginPage {
+  email: string;
+  password: string;
+}
+type ActionLoginPage = { type: 'email' | 'password'; payload: string };
 
 export const initStateLoginPage = {
-    email: '',
-    password: '',
+  email: '',
+  password: '',
+};
+export function reducerLoginPage(
+  state: StateLoginPage,
+  { type, payload }: ActionLoginPage
+) {
+  return (state = { ...state, [type]: payload });
+}
+
+interface StateRegisterPage {
+  name: string;
+  email: string;
+  password: string;
+}
+type ActionRegisterPage = {
+  type: 'name' | ' email' | 'password';
+  payload: string;
 };
 
 export const initStateRegisterPage = {
-    name: '',
-    email: '',
-    password: '',
+  name: '',
+  email: '',
+  password: '',
 };
+
+export function reduserRegisterPage(
+  state: StateRegisterPage,
+  { type, payload }: ActionRegisterPage
+) {
+  return (state = { ...state, [type]: payload });
+}
